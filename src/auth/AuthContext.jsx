8e786372e41f49be0b82fd2e235f7d024ac29e71
SHussain47 <hussain.syed.6732@gmail.com>
2025-10-19 18:42:54 -0400

    feat: Added token logic and test page

---
+++ src/auth/AuthContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const API = import.meta.env.VITE_API;
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [token, setToken] = useState();
+
+  const value = { token };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) throw Error("useAuth can only be used within AuthProvider");
+  return context;
+}
