bb733cfc12f39237605344ed4f86ed6218cb729b
SHussain47 <hussain.syed.6732@gmail.com>
2025-10-19 19:32:35 -0400

    feat: Added register logic|page and route

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const API = import.meta.env.VITE_API;
 
@@ -7,7 +7,22 @@ const AuthContext = createContext();
 export function AuthProvider({ children }) {
   const [token, setToken] = useState();
 
-  const value = { token };
+  // REGISTER LOGIC
+  const register = async (credentials) => {
+    const response = await fetch(`${API}/users/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
+    });
+    const result = await response.json();
+    if (!response) {
+      throw Error(result.message);
+    }
+    setToken(result.token);
+    localStorage.setItem("token", token);
+  };
+
+  const value = { token, register };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
